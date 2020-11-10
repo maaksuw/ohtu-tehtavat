64e71098b3f57571ae91f56e443926bc1267a07b
Pinja Vierimaa <pinja.vierimaa@helsinki.fi>
2020-11-10 20:17:55 +0200

    Tehtävä 5 tehty.

---
+++ viikko3/tehtava5/LoginCucumber/src/test/java/ohtu/Stepdefs.java
@@ -0,0 +1,48 @@
+package ohtu;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.*;
+import ohtu.io.*;
+import ohtu.data_access.*;
+import ohtu.services.*;
+
+public class Stepdefs {
+    App app;
+    StubIO io;
+    UserDao userDao;
+    AuthenticationService auth;
+    List<String> inputLines;
+    
+    @Before
+    public void setup(){
+        userDao = new InMemoryUserDao();
+        auth = new AuthenticationService(userDao);
+        inputLines = new ArrayList<>();      
+    }
+    
+    @Given("^command login is selected$")
+    public void commandLoginSelected() throws Throwable {
+        inputLines.add("login");
+    }
+
+    @When("username {string} and password {string} are entered")
+    public void usernameAndPasswordAreEntered(String username, String password) {
+       inputLines.add(username);
+       inputLines.add(password);
+       
+       io = new StubIO(inputLines); 
+       app = new App(io, auth);
+       app.run();
+    }    
+    
+    @Then("system will respond with {string}")
+    public void systemWillRespondWith(String expectedOutput) {
+        assertTrue(io.getPrints().contains(expectedOutput));
+    }    
+
+}
